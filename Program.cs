a2fb07ae64aca19f856c5e02514b9bee1cd352d8
Liza Tsaturyan <lt291@inlumine.ual.es>
2024-05-21 14:55:28 -0700

    4.2 adding queries

--- Program.cs
+++ Program.cs
@@ -78,6 +78,64 @@ class Program
 
         Console.WriteLine(xml);
         xml.Save("users.xml");
+
+
+        //excercise2
+
+        Console.WriteLine("------------------------------------------------------");
+        XElement usersXml = XElement.Load("users.xml");
+
+        // Query: Get All Users
+        var allUsers = from user in usersXml.Elements("User")
+                       select new
+                       {
+                           Id = (int)user.Element("Id"),
+                           Name = (string)user.Element("Name"),
+                           Category = (string)user.Element("Category")
+                       };
+        
+        Console.WriteLine("All Users:");
+        foreach (var user in allUsers)
+        {
+            Console.WriteLine($"Id: {user.Id}, Name: {user.Name}, Category: {user.Category}");
+        }
+
+        Console.WriteLine("------------------------------------------------------");
+        // Query: Get Users with an "admin" Category
+        var category = "Admin";
+        var usersInCategory = from user in usersXml.Elements("User")
+                              where (string)user.Element("Category") == category
+                              select new
+                              {
+                                  Id = (int)user.Element("Id"),
+                                  Name = (string)user.Element("Name")
+                              };
+
+        Console.WriteLine($"\nUsers in Category {category}:");
+        foreach (var user in usersInCategory)
+        {
+            Console.WriteLine($"Id: {user.Id}, Name: {user.Name}");
+        }
+
+        Console.WriteLine("------------------------------------------------------");
+        // Query: Total Connection Time for Each User
+        var totalConnectionTimePerUser = from user in usersXml.Elements("User")
+                                         let totalConnectionTime = (from app in user.Element("Applications").Elements("Application")
+                                                                    from conn in app.Element("Connections").Elements("Connection")
+                                                                    select (int)conn.Element("Time")).Sum()
+                                         select new
+                                         {
+                                             Name = (string)user.Element("Name"),
+                                             TotalTime = totalConnectionTime
+                                         };
+
+        Console.WriteLine("\nTotal Connection Time per User:");
+        foreach (var user in totalConnectionTimePerUser)
+        {
+            Console.WriteLine($"Name: {user.Name}, TotalTime: {user.TotalTime}");
+        }
+
+
     }
 }
 
